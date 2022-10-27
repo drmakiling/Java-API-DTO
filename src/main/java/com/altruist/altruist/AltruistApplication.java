66498d69f604ce58e534fff791d4be7ac9ce282f
Danielle Makiling <dmakiling@sqasquared.com>
2022-10-27 15:47:08 -0700

    Initial commit

---
+++ src/main/java/com/altruist/altruist/AltruistApplication.java
@@ -0,0 +1,13 @@
+package com.altruist.altruist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltruistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltruistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altruist/altruist/AltruistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altruist.altruist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltruistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
