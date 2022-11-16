bd7ee0f21568d362db0301133af1991059b2febc
Danielle Makiling <dmakiling@sqasquared.com>
2022-11-16 12:16:00 -0800

    added Cookie header

--- src/test/java/Customer.java
+++ src/test/java/Customer.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Cookie;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.apache.commons.configuration.ConfigurationException;
@@ -23,13 +24,24 @@ public class Customer {
         }
     }
     public String token;
+
+    public static Cookie JSESSIONCOOKIE() {
+        Cookie jsessionIDCookie = new Cookie.Builder("JSESSIONID", "BEC7F52B015BD05EE6CCACDEC30EDC80")
+                .setSecured(true)
+                .build();
+
+        return jsessionIDCookie;
+    }
     public void callingsaveProfileAddressAPI() throws IOException,
             ConfigurationException, javax.naming.ConfigurationException {
         prop.load(file);
+        Cookie cookie = JSESSIONCOOKIE();
         RestAssured.baseURI = prop.getProperty("baseUrl");
         Response res =
                 given()
-                        .contentType("application/json")
+                        .relaxedHTTPSValidation()
+                        .headers("Content-Type", "application/json")
+                        .cookie(cookie)
                         .body(
                                 "{\n" +
                                         "  \"addr1\": \"123 Test Street\",\n" +
@@ -53,7 +65,7 @@ public class Customer {
                                 ().response();
 
         JsonPath jsonpath = res.jsonPath();
-        token = jsonpath.get("token");
+        token = jsonpath.get("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImRldi1qd3QtMSJ9.eyJhcmNoZXR5cGUiOiJBRFZJU09SIiwiY2xlYXJpbmdfcGxhdGZvcm0iOiJBTFRSVUlTVF9DTEVBUklORyIsInVzZXJfbmFtZSI6Ijc1NmUxZDQ0LTg5ZmItNGYyNS04OGU4LTlkOWFiMDdkMGIwMCIsIm9yZ19pZCI6IjBiMmNiM2Y2LWIzNTctNGFmZi1hNGU3LTMyZDM3NzFhM2I4YSIsInNjb3BlIjpbInVzZXJzLmFkbWluIl0sImFjdHVhbF91c2VyX25hbWUiOiI3NTZlMWQ0NC04OWZiLTRmMjUtODhlOC05ZDlhYjA3ZDBiMDAiLCJleHAiOjE2Njg2Mzg1MTksIm5vbmNlIjoiNGU1MWZjYzIiLCJhdXRob3JpdGllcyI6WyJhbHRydWlzdF9jbGVhcmluZy51c2VyIiwidXNlcnMuYWRtaW4iLCJvYXV0aC51c2VyIl0sImp0aSI6IjZmNjI5MTE0LTEzNjQtNGQwYS1iZDQwLTg0OTY0MTFmNjEwMSIsImNsaWVudF9pZCI6ImFsdHJ1aXN0LWFwcCJ9.pwMA0uAwm_AQ_cE6EhlCFMIs8aQSGxzZ_F-hU2v1Te9EnJKY_KYTEoc582x7BJAqg6Ba4E3Q1VzpQoBrTeXHZWs-4TgywR0VyD4aI2XhhKgHYkvbN9XLDAYK82SXiVVq0g4jV3JFR0b-X8SCSRQeZMAMUjVYipFC1MM2P04EpZsJhHpNL2PeFuoHuwxdpgQZR0kPINKNfbiIRik_HCzqCglPzacz6Qebmv_90t-ga9-I8ehSydoHnM9aaWz0B9YHShmQmmBu08H6nHVYCHR6QT6OKjAt2ntZrcIzT2LyqN5bWIyNOqwSjVA1sKbxdZxYcVB9rQQTettPuPUS6-hLHQ");
         Utils.setEnvVariable(token);
     }
 }
--- src/test/java/RestAssuredOAuth2.java
+++ src/test/java/RestAssuredOAuth2.java
@@ -1,64 +1,23 @@
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.requestSpecification;
+import org.testng.annotations.BeforeTest;
 
 public class RestAssuredOAuth2 {
+    String oauth_token ;
+    @BeforeTest
+    public void RestAssuredOauth2Test() {
+        oauth_token = RestAssured.given().auth().basic("username", "password")
 
-    public static Response response;
-    private String userAdvisorId = System.getenv("756e1d44-89fb-4f25-88e8-9d9ab07d0b00");
-
-    private String oauth2Payload = "{\n" +
-            "  \"user_id\": \"" + userAdvisorId + "\",\n" +
-            "  \"audience\": \"https://platform.test1.altruistnet.tech\",\n" +
-            "  \"grant_type\": \"user_credentials\",\n" +
-            "  \"scope\": \"user:advisor\" \n}";
-
-    private static String inputUserPayload = "{\n" +
-            "  \"email\": \"qateam+admin+oct12@altruist.com\",\n" +
-            "  \"password\": \"A!tru1st\",\n";
-
-    public void userAdminConfigSetup() {
-        requestSpecification = given().auth().oauth2(getAccessToken(oauth2Payload))
-                .header("Accept", ContentType.JSON.getAcceptHeader())
-                .contentType(ContentType.JSON);
+                .queryParams("client_id", "client_id_value")
+                .queryParams("client_secret", "client_secret_value")
+                .queryParams("grant_type", "authorization_code")
+                .queryParams("redirect_uri", "https://platform.test1.altruistnet.tech")
+                .queryParams("other_params", "sample_param")
+                .when().get("https://platform.test1.altruistnet.tech/oauth2/access_token")
+                .then()
+                .statusCode(200)
+                .extract().header("access_token");
     }
-
-    public String getAccessToken(String payload) {
-        return given()
-                .contentType(ContentType.JSON)
-                .body(payload)
-                .post("/token")
-                .then().extract().response()
-                .jsonPath().getString("access_token");
-    }
-
-    @BeforeAll
-    public static void setup() {
-        RestAssured.baseURI = "https://platform.test1.altruistnet.tech";
-    }
-
-    @Test
-    public void inputUser() {
-        userAdminConfigSetup();
-        response = given(requestSpecification)
-                .body(inputUserPayload)
-                .post("/user")
-                .then().extract().response();
-
-        Assertions.assertEquals(200, response.statusCode());
-    }
-
 }
-
-
-
-
 
 
 
