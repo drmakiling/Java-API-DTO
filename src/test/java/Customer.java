046e7a6ba80dbb03aab902d8ceec5c7df62bf895
Danielle Makiling <dmakiling@sqasquared.com>
2022-11-28 15:05:24 -0800

    fixing Auth Call

--- src/test/java/Customer.java
+++ src/test/java/Customer.java
@@ -24,6 +24,11 @@ public class Customer {
         }
     }
     public String token;
+    public String authorization = "Basic YWx0cnVpc3QtYXBwOmFsdHJ1aXN0LXNlY3JldA==";
+    public String contentType = "application/x-www-form-urlencoded";
+    public String cookie = "JSESSIONID=71F0FC27EEDF66D625C0EB96AF6F525D; JSESSIONID=F918233017B2B8DE349C49EB0DA7C0A5";
+    Object authCookie = cookie;
+
 
     public static Cookie JSESSIONCOOKIE() {
         Cookie jsessionIDCookie = new Cookie.Builder("JSESSIONID", "BEC7F52B015BD05EE6CCACDEC30EDC80")
@@ -32,6 +37,32 @@ public class Customer {
 
         return jsessionIDCookie;
     }
+
+    public void callAuthCall() {
+        Response response =
+                given()
+                        .log()
+                        .all()
+                        .relaxedHTTPSValidation()
+                        .header("Authorization", authorization)
+                        .header("Bearer", token)
+                        .header("Content-Type", contentType)
+                        .cookie((String) authCookie)
+                        .body(
+                                "{\n" +
+                                        "  \"grant_type\": \"password\",\n" +
+                                        "  \"username\": \"qateam+admin+oct12@altruist.com\",\n" +
+                                        "  \"password\": \"A!tru1st\",\n" +
+                                        "}"
+                        )
+                        .when()
+                        .post("https://platform.test1.altruistnet.tech/idp/oauth/token")
+                        .then() .assertThat().statusCode( 200 ).extract
+                                ().response();
+    }
+
+
+
     public void callingsaveProfileAddressAPI() throws IOException,
             ConfigurationException, javax.naming.ConfigurationException {
         prop.load(file);
@@ -39,6 +70,8 @@ public class Customer {
         RestAssured.baseURI = prop.getProperty("baseUrl");
         Response res =
                 given()
+                        .log()
+                        .all()
                         .relaxedHTTPSValidation()
                         .headers("Content-Type", "application/json")
                         .cookie(cookie)
@@ -65,7 +98,7 @@ public class Customer {
                                 ().response();
 
         JsonPath jsonpath = res.jsonPath();
-        token = jsonpath.get("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImRldi1qd3QtMSJ9.eyJhcmNoZXR5cGUiOiJBRFZJU09SIiwiY2xlYXJpbmdfcGxhdGZvcm0iOiJBTFRSVUlTVF9DTEVBUklORyIsInVzZXJfbmFtZSI6Ijc1NmUxZDQ0LTg5ZmItNGYyNS04OGU4LTlkOWFiMDdkMGIwMCIsIm9yZ19pZCI6IjBiMmNiM2Y2LWIzNTctNGFmZi1hNGU3LTMyZDM3NzFhM2I4YSIsInNjb3BlIjpbInVzZXJzLmFkbWluIl0sImFjdHVhbF91c2VyX25hbWUiOiI3NTZlMWQ0NC04OWZiLTRmMjUtODhlOC05ZDlhYjA3ZDBiMDAiLCJleHAiOjE2Njg2Mzg1MTksIm5vbmNlIjoiNGU1MWZjYzIiLCJhdXRob3JpdGllcyI6WyJhbHRydWlzdF9jbGVhcmluZy51c2VyIiwidXNlcnMuYWRtaW4iLCJvYXV0aC51c2VyIl0sImp0aSI6IjZmNjI5MTE0LTEzNjQtNGQwYS1iZDQwLTg0OTY0MTFmNjEwMSIsImNsaWVudF9pZCI6ImFsdHJ1aXN0LWFwcCJ9.pwMA0uAwm_AQ_cE6EhlCFMIs8aQSGxzZ_F-hU2v1Te9EnJKY_KYTEoc582x7BJAqg6Ba4E3Q1VzpQoBrTeXHZWs-4TgywR0VyD4aI2XhhKgHYkvbN9XLDAYK82SXiVVq0g4jV3JFR0b-X8SCSRQeZMAMUjVYipFC1MM2P04EpZsJhHpNL2PeFuoHuwxdpgQZR0kPINKNfbiIRik_HCzqCglPzacz6Qebmv_90t-ga9-I8ehSydoHnM9aaWz0B9YHShmQmmBu08H6nHVYCHR6QT6OKjAt2ntZrcIzT2LyqN5bWIyNOqwSjVA1sKbxdZxYcVB9rQQTettPuPUS6-hLHQ");
+        token = jsonpath.get("token");
         Utils.setEnvVariable(token);
     }
 }
--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -7,7 +7,7 @@ public class RunTest {
     @Test()
     public void saveProfileAddress() throws IOException, ConfigurationException, javax.naming.ConfigurationException {
         Customer customer=new Customer();
-        customer.callingsaveProfileAddressAPI();
+        customer.callAuthCall();
     }
 }
 
